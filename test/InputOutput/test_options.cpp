5798662d1bf34ee75c9eb60ff725367787cca5e4
cheg <egerlanc@physik.hu-berlin.de>
2019-09-07 18:07:14 +0200

    Added tests for filetools

--- test/InputOutput/test_options.cpp
+++ test/InputOutput/test_options.cpp
@@ -5,13 +5,15 @@
 TEST_CASE("Options: get options")
 {
   // simulate starting the program with some options
-  int argc = 5;
+  int argc = 7;
   char *argv[argc];
   argv[0] = (char *)"./../bin/Spike";
   argv[1] = (char *)"--mode";
   argv[2] = (char *)"3";
   argv[3] = (char *)"--file";
   argv[4] = (char *)"test.out";
+  argv[5] = (char *)"--rate";
+  argv[6] = (char *)"1e-2";
 
   // get options struct from "command line options"
   Options *opts;
@@ -30,4 +32,16 @@ TEST_CASE("Options: get options")
     REQUIRE(opts->parameters == "test.out");
   };
 
+  // check if we got the rate right
+  SECTION("Get firing rate")
+  {
+    REQUIRE(opts->rate == true);
+    REQUIRE(opts->dt == 1e-2);
+  };
+
+  // check if we got the isi right
+  SECTION("Get isi")
+  {
+    REQUIRE(opts->isi == false);
+  };
 }
