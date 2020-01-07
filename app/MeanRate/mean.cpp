2df43996ce9ea73a71512395af94490fc53135d3
Christoph Egerland <egerlanc@physik.hu-berlin.de>
2020-01-07 15:18:40 +0100

    mean actually calculates mean rate

--- app/MeanRate/mean.cpp
+++ app/MeanRate/mean.cpp
@@ -5,21 +5,42 @@
 // main
 int main(int argc, char *argv[])
 {
+  //Options options(argc, argv);
 
-  Options options(argc, argv);
+  // define neuron
+  LIF lif(0.5, 0.001);
+  Timeframe time(0.0, 1e4, 1e-2);
 
-  PIF pif(options.get_file());
-  LIF lif(options.get_file());
+  // define adaptation
+  ExpAdaptation adapt(3, 0.5);
+  int Delta_steps = 3;
+  double Delta_vals[Delta_steps] = {0.1, 1, 2, 10};
 
-  Timeframe time(0.0, 1000, 1e-3);
+  // define mu steps
+  double mu = 0.5;
+  double mu_end = 3.5;
+  double dmu = 0.01;
+  int muSteps = (int) ((mu_end-mu)/dmu);
 
-  int count_pif = pif.count(time);
-  int count_lif = lif.count(time);
+  // array to put mean rate into
+  double rate[muSteps][Delta_steps];
 
+  // loop over mu values
+  for(int i = 0; i < muSteps; i++)
+  {
+    mu += dmu;
+    lif.set_mu(mu);
+    std::cout << mu << " " ;
+    std::cout << (double) lif.count(time)/1e4 << " ";
+    for(int j = 0; j < Delta_steps; j++)
+    {
+      adapt.set_Delta(Delta_vals[j]);
+      rate[i][j] = (double) lif.count(time, adapt)/1e4;
+      std::cout << rate[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
 
-  std::cout << "time steps: " << time.get_steps() << std::endl;
-  std::cout << "PIF counts: " << count_pif << std::endl;
-  std::cout << "LIF counts: " << count_lif << std::endl;
 
   return 0;
 };
