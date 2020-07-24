d3a6867cef8284d0cb32e649457c790909bb8d3f
cheg <egerlanc@physik.hu-berlin.de>
2020-07-24 10:26:20 +0200

    Restructed IF classes. Input files are now .ini
    Signed-off-by: cheg <egerlanc@physik.hu-berlin.de>

--- src/Neuron/IF.cpp
+++ src/Neuron/IF.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 
-// json parser
-#include <boost/property_tree/json_parser.hpp>
+// ini parser
+#include <boost/property_tree/ini_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 namespace pt = boost::property_tree;
 
@@ -14,9 +14,9 @@ IF::IF(double mu, double D) : mu(mu), D(D), generator(rd()), dist(0.0, 1.0) {
 }
 
 IF::IF(const std::string &input_file) : generator(rd()), dist(0.0, 1.0) {
-  // read json
+  // read ini
   pt::ptree root;
-  pt::read_json(input_file, root);
+  pt::read_ini(input_file, root);
 
   // read parameters
   this->mu = root.get<double>("Neuron.mu");
@@ -73,7 +73,7 @@ void IF::get_voltage_curve(const TimeFrame &time, std::vector<double> &v) {
   const double dt = time.get_dt();
 
   // perform euler maruyama scheme
-  for (size_t i = 1; i < time.get_steps(); i++) {
+  for (size_t i = 1; i < time.get_size(); i++) {
     v[i] = v[i - 1] + this->drift(v[i - 1]) * dt +
            this->diffusion() * dist(generator) * sqrt(dt);
 
@@ -84,10 +84,35 @@ void IF::get_voltage_curve(const TimeFrame &time, std::vector<double> &v) {
   }
 }
 
-void IF::print_info(std::ofstream &file) const {
-  // print neuron parameters to file
-  file << "# Neuron parameters: "
-       << "\n"
-       << "# mu = " << mu << "\n"
-       << "# D = " << D << "\n";
+std::ostream& operator<< (std::ostream &out, const IF &neuron) {
+  neuron.print(out);
+  return out;
 }
+
+PIF::PIF(double mu, double D) : IF(mu, D) {}
+
+PIF::PIF(const std::string &input_file) : IF(input_file) {
+  // read ini
+  pt::ptree root;
+  pt::read_ini(input_file, root);
+
+  // check if type is right
+  auto type = root.get<std::string>("Neuron.type");
+  assert(type == "PIF");
+}
+
+double PIF::drift(double v) const { return this->mu; }
+
+LIF::LIF(double mu, double D) : IF(mu, D) {}
+
+LIF::LIF(const std::string &input_file) : IF(input_file) {
+  // read ini
+  pt::ptree root;
+  pt::read_ini(input_file, root);
+
+  // check if type is right
+  auto type = root.get<std::string>("Neuron.type");
+  assert(type == "LIF");
+}
+
+inline double LIF::drift(double v) const { return (this->mu - v); }
