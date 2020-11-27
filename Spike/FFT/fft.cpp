447cbab45a584a0abe09584db40b02858ae6759b
cheg <egerlanc@physik.hu-berlin.de>
2020-11-27 10:56:31 +0100

    Remove OpenMP calls from Fourier transform
    Signed-off-by: cheg <egerlanc@physik.hu-berlin.de>

--- Spike/FFT/fft.cpp
+++ Spike/FFT/fft.cpp
@@ -16,13 +16,10 @@ void fft_r2c(const double dt, const std::vector<double> &signal,
   assert(signal_fourier.size() == length / 2 + 1);
 
   // fourier transform the signal
-  fftw_plan p;
-#pragma omp critical
-  p = fftw_plan_dft_r2c_1d(
+  fftw_plan p = fftw_plan_dft_r2c_1d(
       length, const_cast<double *>(signal.data()),
       reinterpret_cast<fftw_complex *>(signal_fourier.data()), FFTW_ESTIMATE);
   fftw_execute(p);
-#pragma omp critical
   fftw_destroy_plan(p);
 
   // normalize the signal correctly
@@ -40,15 +37,12 @@ void fft_c2r(const double dt,
   assert(signal_fourier.size() == length / 2 + 1);
 
   // fourier transform the signal
-  fftw_plan p;
-#pragma omp critical
-  p = fftw_plan_dft_c2r_1d(
+  fftw_plan p = fftw_plan_dft_c2r_1d(
       length,
       reinterpret_cast<fftw_complex *>(
           const_cast<std::complex<double> *>(signal_fourier.data())),
       signal.data(), FFTW_ESTIMATE);
   fftw_execute(p);
-#pragma omp critical
   fftw_destroy_plan(p);
 
   // normalize the signal correctly
